347b7e18e30874ae61f8091ab6725699f605f96f
matsthys <mats.thys@gmail.com>
2020-03-26 21:47:51 +0100

    Feature 1 probeersels
    bezig met redirecten

--- testFeature1/createAccount.php
+++ testFeature1/createAccount.php
@@ -1,50 +1,9 @@
 <?php
 
-    //$user = new CreateAccount();
-    // include_once('config.php'); // connection to database
+    // klasse toevoegen
+    include_once (__DIR__ . "\classes\ClassCreateAccount.php");
 
-    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-        header ("location: index.php"); // vermoedelijk na account aanmaken
-                                        // meteen naar index.php
-        exit;
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // check if email is empty
-        if (empty(trim($_POST["email"]))) {
-            $usernameError = "Please enter username!";
-        } else {
-            $username = trim($_POST["email"]);
-        }
-
-        // check if full name is empty
-        if (empty(trim($_POST["fullName"]))) {
-            $fullNameError = "Please enter full name!";
-        } else {
-            $fullName = trim($_POST["fullName"]);
-        }
-
-        // check if password is empty
-        if (empty(trim($_POST["password"]))) {
-            $passwordError = "Please enter password!";
-        } else {
-            $password = trim($_POST["password"]);
-        }
-
-        // als er waarden zitten in email, fullName en password
-        // (als deze velden zijn ingevuld)
-        if (!empty($email) && !empty($fullName) && !empty($password)) {
-            $conn = new PDO('mysql:host=localhost;dbname=buddyapptest', "root", "");
-            $statement = $conn->prepare("instert into users (user_id, email, fullName, password) values (:user_id, :email, :fullName, :password)");
-        
-            $statement->bindValue(':user_id', $userId); // sql-injectie onschadelijk/onmogelijk maken
-            $statement->bindValue(':email', $email);
-            $statement->bindValue(':fullName', $fullName);
-            $statement->bindValue(':password', $password);
-
-            $statement->execute(); 
-        }
-    }
+    $createAccount = new CreateAccount();
 
 ?>
 <!DOCTYPE html>
@@ -60,7 +19,13 @@
 <body>
     <div id="loginForm">
         <h1>Create account</h1>
-        <form action="POST">
+        
+        <!-- HAD IK EERST ---------- action & method verkeerd geinterpreteerd -->
+        <!-- <form action="POST"> -->
+
+        <!-- VERBETERD -->
+        <form action="createAccount.php" method="POST">
+
             <label for="email" class="label">E-mail</label>
             <input type="text" name="email" id="email" class="textfield">
             <br>
