ece756a4d523156de39d317b842e1cadafd1ea2e
Yaiza Narganez Gomez <yaizang@HOSHI.lan>
2020-03-27 20:03:27 +0100

    created for in html

--- login.php
+++ login.php
@@ -14,6 +14,23 @@ echo $user1->getFirstname();
     <title>Document</title>
 </head>
 <body>
-    
+<div id="loginForm">
+        <h1>Create account</h1>
+        <form action="" method="post" class="">
+        <label for="firstName" class="label">First name</label>
+            <input type="text" name="firstName" id="firstName" class="textfield">
+            <br>
+            <label for="lastName" class="label">Last name</label>
+            <input type="text" name="lastName" id="lastName" class="textfield">
+            <br>
+            <label for="email" class="label">E-mail</label>
+            <input type="text" name="email" id="email" class="textfield">
+            <br>
+            <label for="password" class="label">Password</label>
+            <input type="text" name="password" id="password" class="textfield">
+            <br>
+            <input type="submit" id="submit">
+        </form>
+    </div>
 </body>
 </html>
