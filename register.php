65507225d3b1999765c31f97e45e0863aefe1b15
Yaiza Narganez Gomez <yaizang@HOSHI.lan>
2020-03-28 14:11:07 +0100

    input required form + if not empty set param

--- register.php
+++ register.php
@@ -1,10 +1,16 @@
 <?php
 include_once(__DIR__ . "/classes/User.php");
+
 $user1 = new User();
-if (isset($_POST['firstName'])) {$user1->setFirstname($_POST['firstName']);}
-if (isset($_POST['lastName'])) {$user1->setLastname($_POST['lastName']);}
-if (isset($_POST['email'])) {$user1->setEmail($_POST['email']);}
-if (isset($_POST['password'])) {$user1->setPassword($_POST['password']);}
+
+if (!empty($_POST)) {
+$user1->setFirstname($_POST['firstName']);
+$user1->setLastname($_POST['lastName']);
+$user1->setEmail($_POST['email']);
+$user1->setPassword($_POST['password']);
+
+
+}
 
 $user1->registerUser();
 
@@ -25,16 +31,16 @@ $user1->registerUser();
         <h1>Create account</h1>
         <form action="" method="post" class="">
         <label for="firstName" class="label">First name</label>
-            <input type="text" name="firstName" id="firstname" class="textfield">
+            <input type="text" name="firstName" id="firstname" class="textfield" required>
             <br>
             <label for="lastName" class="label">Last name</label>
-            <input type="text" name="lastName" id="lastname" class="textfield">
+            <input type="text" name="lastName" id="lastname" class="textfield" required>
             <br>
             <label for="email" class="label">E-mail</label>
-            <input type="text" name="email" id="email" class="textfield">
+            <input type="text" name="email" id="email" class="textfield" required>
             <br>
             <label for="password" class="label">Password</label>
-            <input type="text" name="password" id="password" class="textfield">
+            <input type="text" name="password" id="password" class="textfield" required>
             <br>
             <input type="submit" id="submit">
         </form>
---
+++ temporarynotes.php
@@ -0,0 +1,6 @@
+if (isset($_POST['firstName'])) {$user1->setFirstname($_POST['firstName']);}
+if (isset($_POST['lastName'])) {$user1->setLastname($_POST['lastName']);}
+if (isset($_POST['email'])) {$user1->setEmail($_POST['email']);}
+if (isset($_POST['password'])) {$user1->setPassword($_POST['password']);}
+
+
