192b962d61d329bf0ab5fe1ad58cdba09fc5115e
Yaiza Narganez Gomez <yaizang@HOSHI.lan>
2020-04-03 11:47:53 +0200

    gitignore

--- temporarynotes.php
+++
@@ -1,6 +0,0 @@
-if (isset($_POST['firstName'])) {$user1->setFirstname($_POST['firstName']);}
-if (isset($_POST['lastName'])) {$user1->setLastname($_POST['lastName']);}
-if (isset($_POST['email'])) {$user1->setEmail($_POST['email']);}
-if (isset($_POST['password'])) {$user1->setPassword($_POST['password']);}
-
-
