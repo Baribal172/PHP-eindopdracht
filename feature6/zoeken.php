79bfbccf1ae693d4ae3330cfbb97327f96238052
Glenn19990705 <cristiaensglenn@outlook.be>
2020-04-04 14:21:15 +0200

    connectie met databank

--- feature6/zoeken.php
+++ feature6/zoeken.php
@@ -2,5 +2,13 @@
 
 include_once(__DIR__ . "./../index.php");
 
+//variabelen
+$zoekin=$_POST['zoekin'];
+$sorteer=$_POST['sorteerop'];
+
+//connectie met databank
+include_once(__DIR__ . "./../classes/Db.php");
+
+
 
 ?>
