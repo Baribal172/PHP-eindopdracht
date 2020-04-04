913a9bfc943cd935f5628ce981850899e711cbcf
Glenn19990705 <cristiaensglenn@outlook.be>
2020-04-04 13:33:36 +0200

    Aanpassen en toevoegen van opties

--- index.php
+++ index.php
@@ -9,28 +9,41 @@
 
 <body>
 <form action= <?php include_once(__DIR__ . "./../PHP-eindopdracht/feature6/zoeken.php") ?> method="post">
-      <th>Zoek:</th>
-      <th><input type="text" name="zoekterm" id="zoekterm" value="<?php echo $_POST['zoekterm'];   ?>" /></th>
+      <th>Filteren:</th>
+      <th><input type="text" name="zoekterm" id="zoekterm"  value="<?php echo $_POST['zoekterm'];?>" /></th>
       <th ><div>in:</div></th>
       <th >
         <div>
-          <select name="zoekin" size="1" id="zoekin">
-            <option value="alles">Alles</option>
-            <option value="klant_naam">Klanten</option>
-            <option value="opdracht">Opdracht</option>
-            <option value="id">Opdracht-ID</option>
-            <option value="personeelsnaam">Personeel</option>
-            <option value="status">Status</option>
+          <select name="zoekin"  id="zoekin">
+            <option value="designer">Designer</option>
+            <option value="developper">Developper</option>
+  
           </select>
           </div>           </th>
       <th ><div>Sorteer op:</div></th>
       <th><div >
-        <select name="sorteerop" size="1" id="sorteerop">
-          <option value="klant_naam" selected="selected">Klanten</option>
-          <option value="opdracht">Opdracht</option>
-          <option value="id">Opdracht-ID</option>
-          <option value="personeelsnaam">Personeel</option>
-          <option value="status">Status</option>
+        <select name="sorteerop" id="sorteerop">
+        <option value="alles">Alles</option>
+            <option value="name">Naam</option>
+            <option value="gaming">Gaming</option>
+            <option value="technology">Technology</option>
+            <option value="food">Food</option>
+            <option value="fashion">Fashion</option>
+            <option value="animals">Animals</option>
+            <option value="art">Art</option>
+            <option value="nature">Nature</option>
+            <option value="travel">Travel</option>
+            <option value="dancing">Dancing</option>
+            <option value="cosplay">Cosplay</option>
+            <option value="photography">Photography</option>
+            <option value="gadgest">Gadgest</option>
+            <option value="cars">Cars</option>
+            <option value="programming">Programming</option>
+            <option value="singing">Singing</option>
+            <option value="languages">Languages</option>
+            <option value="parties">Parties</option>
+            <option value="fitness">Fitness</option>
+            <option value="writing">Writing</option>
         </select>
       </div></th>
       <th><input type="submit" name="zoek" value="Zoek!" /></th>
