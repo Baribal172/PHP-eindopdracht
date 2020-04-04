4c6918fdec1a182fd071f05ee545713713bb2412
Vincent <vincentvandesompel@hotmail.com>
2020-04-04 13:54:17 +0200

    Form for edit added tot profile.php

--- profile.php
+++ profile.php
@@ -2,16 +2,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
 ?>
 
 
@@ -25,6 +15,29 @@
 </head>
 
 <body>
+    <div id="profileForm">
+        <h1>Edit profile</h1>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="">
+            <label for="firstName" class="label">First name</label>
+            <input type="text" name="firstName" id="firstname" value="<?php echo htmlspecialchars(!empty($_POST["firstName"]) ? $_POST["firstName"] : ''); ?>" class="textfield" required>
+            <br>
+            <label for="lastName" class="label">Last name</label>
+            <input type="text" name="lastName" id="lastname" value="<?php echo htmlspecialchars(!empty($_POST["lastName"]) ? $_POST["lastName"] : ''); ?>" class="textfield" required>
+            <br>
+            <label for="bio" class="label">Bio</label>
+            <input type="text" name="bio" id="bio" value="<?php echo htmlspecialchars(!empty($_POST["bio"]) ? $_POST["bio"] : ''); ?>" class="textfield" required>
+            <br>
+            <label for="email" class="label">E-mail</label>
+            <input type="text" name="email" id="email" value="<?php echo htmlspecialchars(!empty($_POST["email"]) ? $_POST["email"] : ''); ?>" class="textfield" required>
+            <br>
+            <label for="password" class="label">Password</label>
+            <input type="password" name="password" id="password" class="textfield" required>
+            <br>
+            <input type="submit" id="submit">
+        </form>
+    </div>
+
+
 
 </body>
 
