4ed51ea24e3e52a185de05821471ea540e22fb05
Glenn19990705 <cristiaensglenn@outlook.be>
2020-04-04 13:01:54 +0200

    kopie completeProfile.php verwijdert + index.php form toegekend

--- index.php
+++ index.php
@@ -8,7 +8,33 @@
 </head>
 
 <body>
-
+<form action= <?php include_once(__DIR__ . "./../PHP-eindopdracht/feature6/zoeken.php") ?> method="post">
+      <th>Zoek:</th>
+      <th><input type="text" name="zoekterm" id="zoekterm" value="<?php echo $_POST['zoekterm'];   ?>" /></th>
+      <th ><div>in:</div></th>
+      <th >
+        <div>
+          <select name="zoekin" size="1" id="zoekin">
+            <option value="alles">Alles</option>
+            <option value="klant_naam">Klanten</option>
+            <option value="opdracht">Opdracht</option>
+            <option value="id">Opdracht-ID</option>
+            <option value="personeelsnaam">Personeel</option>
+            <option value="status">Status</option>
+          </select>
+          </div>           </th>
+      <th ><div>Sorteer op:</div></th>
+      <th><div >
+        <select name="sorteerop" size="1" id="sorteerop">
+          <option value="klant_naam" selected="selected">Klanten</option>
+          <option value="opdracht">Opdracht</option>
+          <option value="id">Opdracht-ID</option>
+          <option value="personeelsnaam">Personeel</option>
+          <option value="status">Status</option>
+        </select>
+      </div></th>
+      <th><input type="submit" name="zoek" value="Zoek!" /></th>
+    </form>
 </body>
 
 </html>
