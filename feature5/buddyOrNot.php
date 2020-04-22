f313b6940278a7ea57df511b4c34da89d1c83691
Glenn19990705 <cristiaensglenn@outlook.be>
2020-04-22 10:04:11 +0200

    toevoeging classes in file + html

---
+++ feature5/buddyOrNot.php
@@ -0,0 +1,20 @@
+<?php 
+
+include_once(__DIR__ . "./../PHP-eindopdracht/classes/User.php");
+include_once(__DIR__ . "./../PHP-eindopdracht/classes/Db.php");
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
