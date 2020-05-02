afdd890eb0680c32b0c509fafb3eb7933a11b5d5
Baribal172 <vincentvandesompel@hotmail.com>
2020-05-02 23:21:21 +0200

    feature 9: wrote a query to display all the users being buddies

--- home.php
+++ home.php
@@ -6,7 +6,11 @@ if (isset($_SESSION['id'])) {
     $user1 = new User();
     $fetch_data = $user1->fetchUserData();
     $conn = Db::getConnection();
-    $statement = $conn->prepare("SELECT * FROM Buddy WHERE status ='1'");
+    $statement = $conn->prepare("SELECT u1.first_name,u2.first_name
+    FROM (Buddy LEFT OUTER JOIN Users AS u1 ON Buddy.user_one_id = u1.id)
+    LEFT OUTER JOIN Users AS u2 ON Buddy.user_two_id = u2.id
+    WHERE STATUS = '0'");
+    //dont forget to change status to 1, when there are buddies
     $statement->execute();
 ?>  
 <!DOCTYPE html>
@@ -20,7 +24,6 @@ if (isset($_SESSION['id'])) {
 Welcome <b><?php echo $_SESSION['first_name']; ?></b>, You have successfully logged in!<br>
     Your bio is: <?php echo $_SESSION['bio']; ?> <br>
     Click to <a href="./logout.php" class="logout-button">Logout</a>
-
 <?php
 }
 else {
@@ -29,6 +32,10 @@ else {
 <?php
 }
 ?>
-
+<?php 
+        while($row = $statement->fetch()) {?>
+        <h3>This are all the IMD-buddies:</h3>
+        <h4><?php echo $row[0]." and ".$row[1]?></h4>
+        <?php } ?>
 </body>
 </html>
